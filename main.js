e6d44eae46862f5e0265e20baa97412d35fcef1f
Daniyar Orazov <41890934+daniyarorazov@users.noreply.github.com>
2023-03-11 09:13:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 $(document).ready(() => {
-    let hrLinesOrders = document.querySelectorAll('.hrTxtAdd');
-    const addOptionBtn = document.querySelector("#addTxt2Edit");
-
-    function updateHrLinesOrders() {
-        hrLinesOrders = document.querySelectorAll('.hrTxtAdd');
+    const updateHrLinesOrders = () => {
+        // variables
+        let hrLinesOrders = document.querySelectorAll('.hrTxtAdd');
         deleteButton = document.querySelectorAll('.txtoltelsEdit');
         inputCheckboxOption = document.querySelectorAll('.txtChckEdit');
         inputValueOption = document.querySelectorAll('.txtSpecEdit');
         inputHiddenDelete = document.querySelectorAll('.hidden-delete');
 
+        // adding for each elements id as attribute of html-element
         for (let j = 0; j < deleteButton.length; j++) {
             let el = deleteButton[j];
             $(el).attr('id', `txtDltEdit${j+1}`);
@@ -33,37 +32,40 @@ $(document).ready(() => {
             $(el).attr('aria-label', `form-control-sm textyEdit[${k}].ApplianceText`);
         }
 
-        for (let i = 0; i < hrLinesOrders.length; i++) {
-            let el = hrLinesOrders[i];
-            $(el).attr('data-order', i);
-            
-            el.addEventListener('click', () => {
-                if (i != 0 && $(el).attr("class") != "activeHrTxtAdd") {
-                    $(el).css("color", "#000");
-                    $(el).addClass("activeHrTxtAdd");
-                    for (let j of hrLinesOrders) {
-                        if(j != el && ($(j).attr("class")).includes("activeHrTxtAdd")) {
-                            $(j).css("color", "#fff");
-                            $(j).removeClass("activeHrTxtAdd");
-                        }
-                    }
-                }
-            });
-        }
-    }
 
+     
+
+        hrLinesOrders.forEach((el, key) => {
+            $(el).attr('data-order', key);
+            let element = $(el)[0]; 
+            let data = $._data(element); 
+                      
+            if (!(data.events && data.events.click)) {
+                    $(el).click(() => {
+                        const htmlStructureBlock = $(`<div class="input-group editTxt"><a id="txtDltEdit" class="btn btn-danger btn-sm txtoltelsEdit" data-bs-toggle="tooltip" data-bs-placement="left" title="Smazat popisek"><i class="fas fa-close"></i></a><div class="input-group-text"><input class="form-check-input mt-0 txtChckEdit" checked id="applianceTextCheckEdit4" name="textyEdit[4].TextNewsCheck" type="checkbox" value="1" aria-label="Checkbox for following text input"></div><input name="textyEdit[5].ApplianceText" id="applianceTextEdit5" class="form-control form-control-sm txtSpecEdit" type="text" placeholder="Zadejte text..." aria-label="form-control-sm textyEdit[5].ApplianceText"><input type="hidden" id="hiddenDeleteEdit5" class="hidden-delete" name="textyEdit[5].Delete" value="0"></div><hr class="my-1 hrTxtAdd" style="color: white; height: 3px;" data-bs-toggle="tooltip" data-bs-placement="right" title data-order="4">`);
+                        $(htmlStructureBlock).insertAfter(el);
+
+                        if (key != 0 && $(el).attr("class") != "activeHrTxtAdd") {
+                            $(el).addClass("activeHrTxtAdd");
+                            for (let j of hrLinesOrders) {
+                                if(j != el && ($(j).attr("class")).includes("activeHrTxtAdd")) {
+                                    $(j).removeClass("activeHrTxtAdd");
+                                }
+                            }
+                        updateHrLinesOrders();
+
+                        }
+                   
+                    })
+                 
+                }     
+
+                
+
+             
+               
+        });
+    }
     updateHrLinesOrders();
 
-    addOptionBtn.addEventListener('click', function() {
-        const orderHr = document.querySelector('.activeHrTxtAdd');
-        const enterValueInput = $('#applianceTextEdit0').val();
-        const selectedActiveHr = $(orderHr).val(function() {
-            return $(this).attr("data-order");
-        });
-
-        const htmlStructureBlock = $(`<div class="input-group editTxt"><a id="txtDltEdit" class="btn btn-danger btn-sm txtoltelsEdit" data-bs-toggle="tooltip" data-bs-placement="left" title="Smazat popisek"><i class="fas fa-close"></i></a><div class="input-group-text"><input class="form-check-input mt-0 txtChckEdit" checked id="applianceTextCheckEdit4" name="textyEdit[4].TextNewsCheck" type="checkbox" value="1" aria-label="Checkbox for following text input"></div><input value="${enterValueInput}" name="textyEdit[5].ApplianceText" id="applianceTextEdit5" class="form-control form-control-sm txtSpecEdit" type="text" placeholder="Zadejte text..." aria-label="form-control-sm textyEdit[5].ApplianceText"><input type="hidden" id="hiddenDeleteEdit5" class="hidden-delete" name="textyEdit[5].Delete" value="0"></div><hr class="my-1 hrTxtAdd" style="color: white; height: 3px;" data-bs-toggle="tooltip" data-bs-placement="right" title data-order="4">`);
-        $(htmlStructureBlock).insertAfter(selectedActiveHr);
-        updateHrLinesOrders();
-        $('#applianceTextEdit0').val('')
-    });
 });
